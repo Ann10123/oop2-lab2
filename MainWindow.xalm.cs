b43f6b6b4eccd2b8c6e016e79d0d9c09ebb5c4c9
Ann10123 <152207473+Ann10123@users.noreply.github.com>
2025-04-08 00:22:38 +0300

    Update MainWindow.xalm.cs

--- MainWindow.xalm.cs
+++ MainWindow.xalm.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace OOP2
 {
+    public interface ICommandPattern
+    {
+        void Execute();
+    }
     public partial class MainWindow : Window
     {
-        private double firstNumber = 0;
-        private double secondNumber = 0;
-        private bool isOperatorClicked = false;
-        private string currentOperator = "";
+        public double firstNumber = 0;
+        public double secondNumber = 0;
+        public bool isOperatorClicked = false;
+        public string currentOperator = "";
         private bool isMenuOpen = false;
+
         private Stack<CalculatorState> stateHistory = new Stack<CalculatorState>();
         private Stack<CalculatorState> redoStack = new Stack<CalculatorState>();
+
+        private ICommandPattern _undoCommand;
+        private ICommandPattern _redoCommand;
         private void SaveState()
         {
             stateHistory.Push(new CalculatorState
@@ -27,61 +34,6 @@ namespace OOP2
                 IsOperatorClicked = isOperatorClicked
             });
         }
-
-        private void Window_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
-            {
-                SaveState();
-                if (isOperatorClicked)
-                {
-                    TextBoxInput.Clear();
-                    isOperatorClicked = false;
-                }
-                TextBoxInput.Text += (e.Key - Key.NumPad0).ToString();
-            }
-            else if (e.Key == Key.Add)
-            {
-                PlusButton_Click(sender, e);
-            }
-            else if (e.Key == Key.Subtract)
-            {
-                MinusButton_Click(sender, e);
-            }
-            else if (e.Key == Key.Multiply)
-            {
-                MultiplyButton_Click(sender, e);
-            }
-            else if (e.Key == Key.Divide)
-            {
-                DivisionButton_Click(sender, e);
-            }
-            else if (e.Key == Key.Decimal)
-            {
-                SaveState();
-                if (!TextBoxInput.Text.Contains(","))
-                {
-                    TextBoxInput.Text += ",";
-                }
-            }
-            else if (e.Key == Key.Enter)
-            {
-                EqualButton_Click(sender, e);
-            }
-            else if (e.Key == Key.Back)
-            {
-                BackspaceButton_Click(sender, e);
-            }
-            else if (e.Key == Key.C)
-            {
-                ClearButton_Click(sender, e);
-            }
-            else if (e.Key == Key.Escape)
-            {
-                UndoButton_Click(sender, e);
-            }
-        }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             SaveState();
@@ -96,7 +48,6 @@ namespace OOP2
                 TextBoxInput.Text += button.Content.ToString();
             }
         }
-
         private void PlusButton_Click(object sender, RoutedEventArgs e)
         {
             if (double.TryParse(TextBoxInput.Text, out firstNumber))
@@ -106,7 +57,6 @@ namespace OOP2
                 isOperatorClicked = true;
             }
         }
-
         private void MinusButton_Click(object sender, RoutedEventArgs e)
         {
             if (double.TryParse(TextBoxInput.Text, out firstNumber))
@@ -116,7 +66,6 @@ namespace OOP2
                 isOperatorClicked = true;
             }
         }
-
         private void MultiplyButton_Click(object sender, RoutedEventArgs e)
         {
             if (double.TryParse(TextBoxInput.Text, out firstNumber))
@@ -126,7 +75,6 @@ namespace OOP2
                 isOperatorClicked = true;
             }
         }
-
         private void DivisionButton_Click(object sender, RoutedEventArgs e)
         {
             if (double.TryParse(TextBoxInput.Text, out firstNumber))
@@ -181,7 +129,14 @@ namespace OOP2
                 isOperatorClicked = true;
             }
         }
-
+        private void BackspaceButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveState();
+            if (!string.IsNullOrEmpty(TextBoxInput.Text))
+            {
+                TextBoxInput.Text = TextBoxInput.Text[..^1];
+            }
+        }
         private void ClearButton_Click(object sender, RoutedEventArgs e)
         {
             TextBoxInput.Clear();
@@ -190,40 +145,17 @@ namespace OOP2
             secondNumber = 0;
             currentOperator = "";
             stateHistory.Clear();
+            redoStack.Clear();
         }
-
         private void UndoButton_Click(object sender, RoutedEventArgs e)
         {
-            if (stateHistory.Count > 0)
-            {
-                var currentState = new CalculatorState
-                {
-                    Text = TextBoxInput.Text,
-                    FirstNumber = firstNumber,
-                    SecondNumber = secondNumber,
-                    Operator = currentOperator,
-                    IsOperatorClicked = isOperatorClicked
-                };
-
-                redoStack.Push(currentState);
-
-                var previousState = stateHistory.Pop();
-                ApplyState(previousState);
-            }
+            _undoCommand.Execute();
         }
-
         private void RedoButton_Click(object sender, RoutedEventArgs e)
         {
-            if (redoStack.Count > 0)
-            {
-                SaveState(); // поточний стан іде в історію
-
-                var nextState = redoStack.Pop();
-                ApplyState(nextState);
-            }
+            _redoCommand.Execute();
         }
-
-        private void ApplyState(CalculatorState state)
+        public void ApplyState(CalculatorState state)
         {
             TextBoxInput.Text = state.Text;
             firstNumber = state.FirstNumber;
@@ -231,17 +163,6 @@ namespace OOP2
             currentOperator = state.Operator;
             isOperatorClicked = state.IsOperatorClicked;
         }
-
-
-        private void BackspaceButton_Click(object sender, RoutedEventArgs e)
-        {
-            SaveState();
-            if (!string.IsNullOrEmpty(TextBoxInput.Text))
-            {
-                TextBoxInput.Text = TextBoxInput.Text.Substring(0, TextBoxInput.Text.Length - 1);
-            }
-        }
-
         private void EqualButton_Click(object sender, RoutedEventArgs e)
         {
             if (double.TryParse(TextBoxInput.Text, out secondNumber))
@@ -251,53 +172,73 @@ namespace OOP2
 
                 switch (currentOperator)
                 {
-                    case "+":
-                        result = firstNumber + secondNumber;
-                        break;
-                    case "-": 
-                        result = firstNumber - secondNumber;
-                        break;
-                    case "*":
-                        result = firstNumber * secondNumber;
-                        break;
+                    case "+": result = firstNumber + secondNumber; break;
+                    case "-": result = firstNumber - secondNumber; break;
+                    case "*": result = firstNumber * secondNumber; break;
                     case "/":
-                        if (secondNumber != 0)
-                            result = firstNumber / secondNumber;
-                        else
+                        if (secondNumber == 0)
                         {
                             MessageBox.Show("На нуль ділити не можна!");
                             return;
                         }
+                        result = firstNumber / secondNumber;
                         break;
-                    case "nʸ":
-                        result = Math.Pow(firstNumber, secondNumber);
-                        break;
+                    case "nʸ": result = Math.Pow(firstNumber, secondNumber); break;
                     case "log":
-                        if (firstNumber > 0)
-                            result = Math.Log10(firstNumber);
-                        else
+                        if (firstNumber <= 0)
                         {
-                            MessageBox.Show("Логарифм можна обчислити тільки для чисел більше 0!");
+                            MessageBox.Show("Логарифм можливий тільки для чисел більше 0!");
+                            return;
                         }
-                        break;
-                    case "e":
-                        result = Math.E * firstNumber;
-                        break;
-                    case "π":
-                        result = firstNumber * Math.PI;
-                        break;
+                        result = Math.Log10(firstNumber); break;
                     case "√x":
-                        if (firstNumber >= 0)
-                            result = Math.Sqrt(firstNumber);
-                        else
+                        if (firstNumber < 0)
                         {
-                            MessageBox.Show("Не можна знайти корінь з від’ємного числа!");
+                            MessageBox.Show("Не можна взяти корінь з від’ємного числа!");
+                            return;
                         }
-                        break;
+                        result = Math.Sqrt(firstNumber); break;
+                    case "e": result = firstNumber * Math.E; break;
+                    case "π": result = firstNumber * Math.PI; break;
                 }
                 TextBoxInput.Text = result.ToString();
                 firstNumber = result;
                 isOperatorClicked = false;
+            }
+        }
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
+            {
+                SaveState();
+                if (isOperatorClicked)
+                {
+                    TextBoxInput.Clear();
+                    isOperatorClicked = false;
+                }
+                TextBoxInput.Text += (e.Key - Key.NumPad0).ToString();
+            }
+            else if (e.Key == Key.Add) PlusButton_Click(sender, e);
+            else if (e.Key == Key.Subtract) MinusButton_Click(sender, e);
+            else if (e.Key == Key.Multiply) MultiplyButton_Click(sender, e);
+            else if (e.Key == Key.Divide) DivisionButton_Click(sender, e);
+            else if (e.Key == Key.Decimal)
+            {
+                SaveState();
+                if (!TextBoxInput.Text.Contains(",")) TextBoxInput.Text += ",";
+            }
+            else if (e.Key == Key.P) PiButton_Click(sender, e);
+            else if (e.Key == Key.S) SqrtButton_Click(sender, e);
+            else if (e.Key == Key.E) EpsButton_Click(sender, e);
+            else if (e.Key == Key.L) LogButton_Click(sender, e);
+            else if (e.Key == Key.D) PowerButton_Click(sender, e);
+            else if (e.Key == Key.Enter) EqualButton_Click(sender, e);
+            else if (e.Key == Key.Back) BackspaceButton_Click(sender, e);
+            else if (e.Key == Key.C) ClearButton_Click(sender, e);
+            else if (e.Key == Key.Escape) UndoButton_Click(sender, e);
+            else if (e.Key == Key.Y && (Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control)
+            {
+                RedoButton_Click(sender, e);
             }
         }
         private void BurgerButton_Click(object sender, RoutedEventArgs e)
@@ -305,7 +246,6 @@ namespace OOP2
             ExtraButtonsColumn.Width = new GridLength(1, GridUnitType.Star);
             SetAdditionalButtonsVisibility(Visibility.Visible);
             isMenuOpen = true;
-
             ButtonBurger.Visibility = Visibility.Collapsed;
             ButtonBurger2.Visibility = Visibility.Visible;
         }
@@ -314,7 +254,6 @@ namespace OOP2
             ExtraButtonsColumn.Width = new GridLength(0);
             SetAdditionalButtonsVisibility(Visibility.Collapsed);
             isMenuOpen = false;
-
             ButtonBurger2.Visibility = Visibility.Collapsed;
             ButtonBurger.Visibility = Visibility.Visible;
         }
@@ -325,19 +264,12 @@ namespace OOP2
             ButtonPower.Visibility = visibility;
             ButtonLog.Visibility = visibility;
             ButtonEps.Visibility = visibility;
-            ButtonRedo.Visibility = visibility;
         }
         public MainWindow()
         {
             InitializeComponent();
+            _undoCommand = new UndoCommand(this, stateHistory, redoStack);
+            _redoCommand = new RedoCommand(this, stateHistory, redoStack);
         }
-    }
-    public class CalculatorState
-    {
-        public string Text { get; set; }
-        public double FirstNumber { get; set; }
-        public double SecondNumber { get; set; }
-        public string Operator { get; set; }
-        public bool IsOperatorClicked { get; set; }
     }
 }
